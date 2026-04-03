e31869c5a82bc5657c95b8e03078b4a84addbd5b
SuperPuperMegaUltraGovnovoz <168558432+SuperPuperMegaUltraGovnovoz@users.noreply.github.com>
2026-04-03 21:59:11 +0700

    Add files via upload

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,52 +13,61 @@ public class Main {
         SetTargetFPS(-1);
 
         Rectangle world = new Rectangle().x(300).y(300).width(40).height(40);
-        Object object = new Object((int)world.x(), (int)world.y(), 60, 30);
+        Object object = new Object((int)world.x(), (int)world.y(), 60, 60);
 
         Camera2D camera = new Camera2D();
         camera.target(new Vector2().x(world.x() + 20).y(world.y() + 20));
         camera.offset(new Vector2().x(screenWidth1/2).y(screenHeight1/2));
         camera.rotation(0.0f);
         camera.zoom(1.0f);
-        float velosity = 0.2f;
+        float velosityX = 0.2f;
+        float velosityX1 = 0.2f;
+        float velosityY = 0.2f;
+        float velosityY1 = 0.2f;
 
-        Player player = new Player((int)camera.target().x(), (int)camera.target().y(), 15, 20, false);
+        Player player = new Player((int)camera.target().x(), (int)camera.target().y(), 30, 40, false);
 
         while (!WindowShouldClose()) {
-//            if (IsMouseButtonDown(MOUSE_BUTTON_MIDDLE)){
-//                camera.target(Vector2Add(camera.target(), Vector2Scale(GetMouseDelta(), -1.0f/camera.zoom())));
-//            }
+            if(player.onFoor){
+                velosityY1 = 0;
+            }else{velosityY1 = 0.2f;}
+            if(player.CollisionWithUp){
+                velosityY = 0;
+            }else{velosityY = 0.2f;}
+            if(player.CollisionWithLeft){
+                velosityX = 0;
+            }else{velosityX = 0.2f;}
+            if(player.CollisionWithRight){
+                velosityX1 = 0;
+            }else{velosityX1 = 0.2f;}
+
             //if(player.onFoor) {
-            if(!player.CollisionWithUp) {
-                if (IsKeyDown(KEY_W)) {
-                    camera.target().y(camera.target().y() + (-velosity) * GetFrameTime() * 1000);
-                }
+            if (IsKeyDown(KEY_W)) {
+                camera.target().y(camera.target().y() + (-velosityY) * GetFrameTime() * 1000);
             }
             //}
-            if(!player.onFoor) {
-                if (IsKeyDown(KEY_S)) {
-                    camera.target().y(camera.target().y() + velosity * GetFrameTime() * 1000);
-                }
+
+            if (IsKeyDown(KEY_S)) {
+                camera.target().y(camera.target().y() + velosityY1 * GetFrameTime() * 1000);
             }
+
            // if(!player.onFoor){
              //   camera.target().y(camera.target().y() + 0.1f * GetFrameTime() * 1000);
             //}
-            if(!player.CollisionWithLeft) {
-                if (IsKeyDown(KEY_A)) {
-                    camera.target().x(camera.target().x() + (-velosity) * GetFrameTime() * 1000);
-                }
+
+            if (IsKeyDown(KEY_A)) {
+                camera.target().x(camera.target().x() + (-velosityX) * GetFrameTime() * 1000);
             }
-            if(!player.CollisionWithRight) {
-                if (IsKeyDown(KEY_D)) {
-                    camera.target().x(camera.target().x() + velosity * GetFrameTime() * 1000);
-                }
+
+
+            if (IsKeyDown(KEY_D)) {
+                camera.target().x(camera.target().x() + velosityX1 * GetFrameTime() * 1000);
             }
+
             player.x = (int)camera.target().x();
             player.y = (int)camera.target().y();
             camera.zoom((camera.zoom()) + (GetMouseWheelMove() * 0.03f));
-            if (camera.zoom() <= 0) {
-                camera.zoom(0.099999f);
-            }
+            camera.zoom(Math.max(camera.zoom(), 0.02f));
 
             BeginDrawing();
             ClearBackground(RAYWHITE);
